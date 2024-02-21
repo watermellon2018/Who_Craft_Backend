f645c6c0e6bc7f0b366883a1000443ec3e746ec7
Kate <watermellon2018@gmail.com>
2024-02-21 18:14:54 +0500

    fixed api paths

--- w_craft_back/characters/display_tree/urls.py
+++ w_craft_back/characters/display_tree/urls.py
@@ -4,7 +4,7 @@ from w_craft_back.views.character_views import CharacterTree,\
     create_character, rename_character
 
 urlpatterns = [
-    path('characters/', CharacterTree.as_view(), name='characters'),
+    path('select/', CharacterTree.as_view(), name='characters'),
     path('delete/', CharacterTree.as_view(), name='delete_character'),
     path('create/', create_character, name='create_character'),
     path('rename/', rename_character, name='rename_character'),
