b6e889f612727d40e045ee0262caa72b8d6af5a5
Kate <watermellon2018@gmail.com>
2024-02-24 16:05:24 +0500

    запрос на отображение все проектов

--- w_craft_back/movie/project/models.py
+++ w_craft_back/movie/project/models.py
@@ -2,7 +2,7 @@ from w_craft_back.movie.properties.models import Genre, Audience
 
 from django.db import models
 
-from PIL import Image
+
 class Project(models.Model):
     title = models.CharField(max_length=255)
     image = models.ImageField(upload_to='project/poster/')  # Поле для загрузки изображения
@@ -14,3 +14,22 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+
+# import base64
+# from rest_framework import serializers
+#
+# class ProjectSerializer(serializers.ModelSerializer):
+#     image_data = serializers.SerializerMethodField()
+#
+#     class Meta:
+#         model = Project
+#         fields = ['id', 'title', 'image', 'format', 'description', 'annot']
+#
+#     def get_image_data(self, obj):
+#         if obj.image:
+#             try:
+#                 with open(obj.image.path, "rb") as img_file:
+#                     return base64.b64encode(img_file.read()).decode('utf-8')
+#             except FileNotFoundError:
+#                 return None
+#         return None
