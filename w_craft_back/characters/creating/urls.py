ffa8b290d5ba50757f18a62efb0d7f28d7b5c3b3
Kate <watermellon2018@gmail.com>
2024-03-14 00:10:37 +0500

    удаление персонажа

--- w_craft_back/characters/creating/urls.py
+++ w_craft_back/characters/creating/urls.py
@@ -1,11 +1,12 @@
-from w_craft_back.characters.creating.view import create_hero, select_all, select_hero_by_id
+from w_craft_back.characters.creating.view import create_hero, select_all,\
+    select_hero_by_id, delete_hero_by_id
 from django.urls import path
 
 
 urlpatterns = [
     path('select_by_id/', select_hero_by_id, name='select_hero_by_id'),
-    # path('delete/', CharacterTree.as_view(), name='delete_hero'),
+    path('delete_by_id/', delete_hero_by_id, name='delete_hero_by_id'),
     path('create/', create_hero, name='create_hero'),
     path('select/', select_all, name='select_all'),
-    # path('rename/', rename_character, name='rename_hero'),
+    # path('update/', rename_character, name='rename_hero'),
 ]
