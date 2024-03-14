8eba7f243c23a0e91bddafd5460071559ad5aa73
Kate <watermellon2018@gmail.com>
2024-03-14 23:31:17 +0500

    Обновление компетенций, характеристик идентификации и внутренние характеристики персонажа

--- w_craft_back/characters/creating/urls.py
+++ w_craft_back/characters/creating/urls.py
@@ -1,5 +1,8 @@
 from w_craft_back.characters.creating.view import create_hero, select_all,\
-    select_hero_by_id, delete_hero_by_id, update_personal_info
+    select_hero_by_id, delete_hero_by_id
+from w_craft_back.characters.creating.view_for_update import update_personal_info,\
+    update_motivate_info, update_inside_data_hero, update_competition_data_hero,\
+    update_identity_data_hero
 from django.urls import path
 
 
@@ -9,4 +12,8 @@ urlpatterns = [
     path('create/', create_hero, name='create_hero'),
     path('select/', select_all, name='select_all'),
     path('update_personal_info/', update_personal_info, name='update_personal_info'),
+    path('update_motivate_info/', update_motivate_info, name='update_motivate_info'),
+    path('update_inside_info/', update_inside_data_hero, name='update_inside_data_hero'),
+    path('update_competition_info/', update_competition_data_hero, name='update_competition_data_hero'),
+    path('update_identity_info/', update_identity_data_hero, name='update_identity_data_hero'),
 ]
