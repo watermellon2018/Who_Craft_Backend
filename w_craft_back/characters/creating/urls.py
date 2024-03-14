557a7147fa4804ef705620ccd16453208bf917bb
Kate <watermellon2018@gmail.com>
2024-03-14 17:47:30 +0500

    Обновление личной информации героя

--- w_craft_back/characters/creating/urls.py
+++ w_craft_back/characters/creating/urls.py
@@ -1,5 +1,5 @@
 from w_craft_back.characters.creating.view import create_hero, select_all,\
-    select_hero_by_id, delete_hero_by_id
+    select_hero_by_id, delete_hero_by_id, update_personal_info
 from django.urls import path
 
 
@@ -8,5 +8,5 @@ urlpatterns = [
     path('delete_by_id/', delete_hero_by_id, name='delete_hero_by_id'),
     path('create/', create_hero, name='create_hero'),
     path('select/', select_all, name='select_all'),
-    # path('update/', rename_character, name='rename_hero'),
+    path('update_personal_info/', update_personal_info, name='update_personal_info'),
 ]
