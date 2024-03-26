91644cb276f460a29c955df24f995752aea6157d
Kate <watermellon2018@gmail.com>
2024-03-26 17:54:30 +0500

    task 16. add edit gen page

--- w_craft_back/movie/poster/views.py
+++ w_craft_back/movie/poster/views.py
@@ -1,6 +1,7 @@
 
 import base64
 import logging
+import requests
 
 from django.http import JsonResponse, HttpResponse
 from django.core.files.base import ContentFile
@@ -8,7 +9,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 
-from w_craft_back.views import create_image_from_string, img2response
+from w_craft_back.views import create_image_from_string, img2response, query_model_hub
 
 logger = logging.getLogger(__name__)
 @api_view(['GET'])
@@ -26,5 +27,31 @@ def generate_poster(request):
 
     image = create_image_from_string(prompt_global)
     response = img2response(image)
+
+    return response
+
+
+@api_view(['POST'])
+def edite_generative_poster(request):
+    params = request.data['data']
+
+    desc = params['correction']
+    img_url = params['image']
+    logger.info('Param cut')
+
+    prompt_global = 'Edit this input image according to the following text description, ' \
+                    'while preserving the overall look and feel of the original image: '
+
+    prompt_desc = desc
+    prompt_global += prompt_desc
+
+    logger.info(f'Prompt edit generative poster: ${prompt_global}')
+
+    img_url = img_url.replace('data:image/png;base64,', '')
+    img_bytes = base64.b64decode(img_url)
+
+    response = query_model_hub(img_bytes, prompt_global)
+    if response.status_code == 200:
+        logger.info('Image was edited.')
 
     return response
