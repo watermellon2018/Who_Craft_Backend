724d4e1514a761cb1b0a3156f49e9525117ec3cc
Kate <watermellon2018@gmail.com>
2024-03-30 14:25:50 +0500

    20_feature. Возможность обновления информации о проекте

--- w_craft_back/movie/project/views.py
+++ w_craft_back/movie/project/views.py
@@ -60,7 +60,6 @@ def delete_project(request):
     try:
         logger.info('Удаление проекта')
         id = request.GET.get('id')
-        # todo:: add user id
         project = Project.objects.get(id=id, user=cur_user)
         project.delete()
         logger.info('Проект удален!')
@@ -72,6 +71,89 @@ def delete_project(request):
         return JsonResponse({'error': str(e)}, status=500)
 
     return HttpResponse(status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def select_project_info(request):
+    user_token = request.GET.get('token_user')
+    cur_user = UserKey.objects.get(key=user_token)
+
+
+    try:
+        logger.info('Select запрос проекта')
+        id = request.GET.get('id')
+        project = Project.objects.get(id=id, user=cur_user)
+        logger.info(f'Проект найден id: {id}')
+        img_obj = None
+        if not project.image == '':
+            with open(project.image.path, "rb") as img_file:
+                img_obj = base64.b64encode(img_file.read()).decode('utf-8')
+                logger.info('Постер найден')
+
+        response = {
+            'id': project.id,
+            'title': project.title,
+            'genre': [genre.translit for genre in project.genre.all()],
+            'format': project.format,
+            'audience': [aud.name for aud in project.audience.all()],
+            'annot': project.annot,
+            'desc': project.desc,
+            'src': img_obj,
+        }
+        return JsonResponse(response, safe=False, status=200)
+
+    except Project.DoesNotExist:
+        return JsonResponse({'error': 'Object with specified ID does not exist'}, status=404)
+
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
+
+
+@api_view(['POST'])
+def update_info_project(request):
+    logger.info('Обновить информацию о проекте')
+    data = request.data['data']
+    user_token = data['token_user']
+    cur_user = UserKey.objects.get(key=user_token)
+
+    try:
+        id = data['id']
+        project = Project.objects.get(id=id, user=cur_user)
+        logger.info(f'Проект найден id: {id}')
+
+        if not data['image'] == '':
+            title = data['title']
+            logger.info('Пользователь загрузил постер для своего проекта')
+            image_data = data['image']
+            format, imgstr = image_data.split(';base64,')
+            ext = format.split('/')[-1]
+            image_data = ContentFile(base64.b64decode(imgstr),
+                                     name='{}.{}'.format(title, ext))  # TODO:: add user id!
+            project.src = image_data
+
+        project.title = data['title']
+        project.format = data['format']
+        project.annot = data['annot']
+        project.desc = data['desc']
+
+        audience_list: list = data['audience']
+        audience_objs = Audience.objects.filter(name__in=audience_list)
+        project.audience.set(audience_objs)
+
+        genre_list: list = data['genre']
+        print(genre_list)
+        genre_objs = Genre.objects.filter(translit__in=genre_list)
+        project.genre.set(genre_objs)
+
+        project.save()
+
+        return HttpResponse(status=200)
+
+    except Project.DoesNotExist:
+        return JsonResponse({'error': 'Object with specified ID does not exist'}, status=404)
+
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
 
 
 class ProjectView(APIView):
@@ -106,7 +188,7 @@ class ProjectView(APIView):
                      'desc': desc,
                      'user': cur_user,
                      }
-        if 'image' in data.keys():
+        if not data['image'] == '':
             logger.info('Пользователь загрузил постер для своего проекта')
             image_data = data['image']
             format, imgstr = image_data.split(';base64,')
@@ -117,13 +199,11 @@ class ProjectView(APIView):
 
         obj = Project.objects.create(**arguments)
 
+        audience_objs = Audience.objects.filter(name__in=audience_list)
+        obj.audience.set(audience_objs)
 
         genre_objs = Genre.objects.filter(translit__in=genre_list)
         obj.genre.set(genre_objs)
 
-        audience_objs = Audience.objects.filter(name__in=audience_list)
-        obj.audience.set(audience_objs)
-
         logger.info('Проект создан!')
         return JsonResponse({'project_id': obj.id}, status=200)
-        # return HttpResponse(status=status.HTTP_200_OK)
